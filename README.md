853830b4bb2dbc3a3c35be7a480f99017ed8d50f
Tetiana Podleska <podleskatetiana@gmail.com>
2023-03-30 13:37:00 +0300

    make markup for section conditions

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# insuranse
-Making a layout
+# gulp2023
+
+Gulp assembly 2023 by lesson from youtube channel from 0 to 1
+
+npm init
+
+npm i gulp-cli -g the first-first gulp on the computer
---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+const { src, dest, watch, parallel, series } = require("gulp");
+
+const scss = require("gulp-sass")(require("sass"));
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify-es").default;
+const browserSync = require("browser-sync").create();
+const autoprefixer = require("gulp-autoprefixer");
+const clean = require("gulp-clean");
+
+function scripts() {
+  return src([
+    // "node_modules/swiper/swiper-bundle.js",
+    "app/js/main.js",
+
+    // 'app/js/*.js',
+    // '!app/js/main.min.js' to exclude files
+  ])
+    .pipe(concat("main.min.js"))
+    .pipe(uglify())
+    .pipe(dest("app/js"))
+    .pipe(browserSync.stream());
+}
+
+function styles() {
+  return src(["app/scss/style.scss", "app/scss/reset.scss"])
+    .pipe(autoprefixer({ overrideBrowserslist: ["last 10 version"] }))
+    .pipe(concat("style.min.css"))
+    .pipe(scss({ outputStyle: "compressed" }))
+    .pipe(dest("app/css"))
+    .pipe(browserSync.stream());
+}
+
+function watcher() {
+  watch(["app/scss/style.scss"], styles);
+  watch(["app/js/main.js"], scripts);
+  watch(["app/**/*.html"]).on("change", browserSync.reload);
+}
+
+function browsersync() {
+  browserSync.init({
+    server: {
+      baseDir: "app/",
+    },
+  });
+}
+
+function cleanDist() {
+  return src("dist").pipe(clean());
+}
+
+function buiding() {
+  return src(["app/css/style.min.css", "app/js/main.min.js", "app/**/*.html"], {
+    base: "app",
+  }).pipe(dest("dist"));
+}
+
+exports.styles = styles;
+exports.scripts = scripts;
+exports.watcher = watcher;
+exports.browsersync = browsersync;
+
+exports.build = series(cleanDist, buiding);
+
+exports.default = parallel(styles, scripts, browsersync, watcher);
